74c666d1f75eb7225f262476b827cfa0f2fa6918
Tyrell <tysteg14@gmail.com>
2023-05-01 19:33:44 -0500

    creates

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Tyrell is the champion";
+Element.append(newHeader)
+
+
+
